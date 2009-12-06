4732bc1c3cd07ccd599c89c7f0679ceaec7bc845
gbence <gbence@defiant.gb>
2009-12-06 22:50:08 +0100

    Implementing Array#average.

--- ext/variance_ext.c
+++ ext/variance_ext.c
@@ -11,7 +11,26 @@ void Init_variance_ext() {
 }
 
 VALUE method_array_average(VALUE self) {
-  int x = 10;
-  return INT2NUM(x);
+  long int i;
+  double sum = 0.0;
+  long int size = RARRAY_LEN(self);
+  VALUE *element = RARRAY_PTR(self);
+
+  for (i=0; i<size; i++) {
+    switch (TYPE(*element)) {
+      case T_FIXNUM:
+        sum += rb_num2long(*element);
+        break;
+      case T_FLOAT:
+        sum += rb_num2dbl(*element);
+        break;
+      default:
+        rb_raise(rb_eTypeError, "not valid value");
+        break;
+    }
+    element++;
+  }
+
+  return rb_float_new(sum / size);
 }
 
--- spec/average_spec.rb
+++ spec/average_spec.rb
@@ -13,3 +13,23 @@ describe "The array [ 1, 2, 3, 4, 5 ]" do
     @a.average.should == 3.0
   end
 end
+
+describe "The array [ 1, 2.0, 3, 4.0 ]" do
+  before :each do
+    @a = [ 1, 2.0, 3, 4.0 ]
+  end
+
+  it "should have an average that equals to 2.5" do
+    @a.average.should == 2.5
+  end
+end
+
+describe "The array [ 1, 2, 'a' ]" do
+  before :each do
+    @a = [ 1, 2, 'a' ]
+  end
+
+  it "should not have an average" do
+    lambda { @a.average }.should raise_error
+  end
+end
