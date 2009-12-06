661d03f665479e73166c171a3849a1d92d0deeff
gbence <gbence@defiant.gb>
2009-12-06 23:11:41 +0100

    Implementing Array#variance.

--- ext/variance_ext.c
+++ ext/variance_ext.c
@@ -1,19 +1,20 @@
 #include "ruby.h"
 
-VALUE ArrayClass = Qnil;
-
 void Init_variance_ext();
 
 VALUE method_array_average(VALUE self);
+VALUE method_array_variance(VALUE self);
 
 void Init_variance_ext() {
   rb_define_method(rb_cArray, "average", method_array_average, 0);
+  rb_define_method(rb_cArray, "variance", method_array_variance, 0);
 }
 
-VALUE method_array_average(VALUE self) {
+double sum_of_rbArray(VALUE self) {
   long int i;
-  double sum = 0.0;
   long int size = RARRAY_LEN(self);
+  double sum = 0.0;
+
   VALUE *element = RARRAY_PTR(self);
 
   for (i=0; i<size; i++) {
@@ -31,6 +32,46 @@ VALUE method_array_average(VALUE self) {
     element++;
   }
 
-  return rb_float_new(sum / size);
+  return(sum);
 }
 
+double average_of_rbArray(VALUE self) {
+  return( sum_of_rbArray(self) / RARRAY_LEN(self) );
+}
+
+double variance_of_rbArray(VALUE self) {
+  long int i;
+  long int size = RARRAY_LEN(self);
+  double sum = 0.0;
+  double average = average_of_rbArray(self);
+  double e;
+
+  VALUE *element = RARRAY_PTR(self);
+
+  for (i=0; i<size; i++) {
+    switch (TYPE(*element)) {
+      case T_FIXNUM:
+        e = rb_num2long(*element);
+        sum += (e-average)*(e-average);
+        break;
+      case T_FLOAT:
+        e = rb_num2dbl(*element);
+        sum += (e-average)*(e-average);
+        break;
+      default:
+        rb_raise(rb_eTypeError, "not valid value");
+        break;
+    }
+    element++;
+  }
+
+  return(sum / size);
+}
+
+VALUE method_array_average(VALUE self) {
+  return rb_float_new( average_of_rbArray(self) );
+}
+
+VALUE method_array_variance(VALUE self) {
+  return rb_float_new( variance_of_rbArray(self) );
+}
--- spec/variance_spec.rb
+++ spec/variance_spec.rb
@@ -13,3 +13,23 @@ describe "The array [ 1, 2, 3, 4, 5 ]" do
     @a.variance.should == 2.0
   end
 end
+
+describe "The array [ 1, 2.0, 3, 4.0 ]" do
+  before :each do
+    @a = [ 1, 2.0, 3, 4.0 ]
+  end
+
+  it "should have a variance that equals to 2.3125" do
+    @a.variance.should == 1.25
+  end
+end
+
+describe "The array [ 1, 2, 'a' ]" do
+  before :each do
+    @a = [ 1, 2, 'a' ]
+  end
+
+  it "should not have a variance" do
+    lambda { @a.variance }.should raise_error
+  end
+end
